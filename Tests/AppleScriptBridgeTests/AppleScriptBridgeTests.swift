ac3d2509e15b2da6c62ee783e86900996bdb401c
Emory Dunn <emory@emorydunn.com>
2023-05-17 14:01:46 -0700

    Add initial tests

--- Tests/AppleScriptBridgeTests/AppleScriptBridgeTests.swift
+++ Tests/AppleScriptBridgeTests/AppleScriptBridgeTests.swift
@@ -2,10 +2,80 @@ import XCTest
 @testable import AppleScriptBridge
 
 final class AppleScriptBridgeTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(AppleScriptBridge().text, "Hello, World!")
-    }
+
+	// MARK: - Convenience Wrappers
+	func testExecuteThrows() {
+		guard let script = NSAppleScript(source: "return 42") else {
+			XCTFail()
+			return
+		}
+
+		XCTAssertNoThrow(try script.execute())
+	}
+
+	func testExecuteThrowsError() {
+
+		guard let script = NSAppleScript(source: "error number 42") else {
+			XCTFail()
+			return
+		}
+
+		XCTAssertThrowsError(try script.execute())
+
+	}
+
+	func testCallHander() {
+		let source = """
+		on runTest()
+		return 42
+		end runTest
+		"""
+
+		guard let script = NSAppleScript(source: source) else {
+			XCTFail()
+			return
+		}
+
+		XCTAssertNoThrow(try script.execute(scriptHandler: "runTest", args: [.null()]))
+
+	}
+
+	// MARK: - Representable Methods
+
+	func testExecuteReturnOptional() throws {
+		guard let script = NSAppleScript(source: "return 42") else {
+			XCTFail()
+			return
+		}
+
+		let result: Int? = try script.execute()
+
+		XCTAssertEqual(result, 42)
+	}
+
+	func testExecuteReturnType() throws {
+		guard let script = NSAppleScript(source: "return 42") else {
+			XCTFail()
+			return
+		}
+
+		XCTAssertEqual(try script.executeThrowingOnNil(), 42)
+	}
+
+	func testExecuteReturnOptionalType() {
+		guard let script = NSAppleScript(source: "return null") else {
+			XCTFail()
+			return
+		}
+
+		do {
+			let result: String = try script.executeThrowingOnNil()
+			XCTFail("Should have thrown an error, not \(result)")
+		} catch {
+
+		}
+	}
+
+	
 }
+
